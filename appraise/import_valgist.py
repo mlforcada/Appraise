556e03a4b8e245cdf952be5fdedeaa5eabf1082e
mlforcada <mlf@dlsi.ua.es>
2017-04-05 19:13:19 +0100

    importing multiple tasks

--- appraise/import_valgist.py
+++ appraise/import_valgist.py
@@ -55,13 +55,8 @@ if __name__ == "__main__":
     PROJECT_HOME = os.path.normpath(os.getcwd() + "/..")
     sys.path.append(PROJECT_HOME)
 
-    # I have to change this to import the right code
-    # We have just added appraise to the system path list, hence this works.
-    # Oh well, there is a problem related to settings.
     from appraise.evaluation.models import EvaluationTask #, validate_source_xml_file #, EvaluationItem
-    # Was: from appraise.wmt14.models import HIT
-    from eval.validatordocgisting import validate_task_xml_file # seems to work
-    # Was: from appraise.wmt14.validators import validate_hits_xml_file
+    # from eval.validatordocgisting import validate_task_xml_file # not used
     
     # We might potentially be dealing with more than a single input file.
     first_run = True
@@ -88,53 +83,26 @@ if __name__ == "__main__":
         _errors = 0
         _total = 0
         _tree = fromstring(task_xml_string.encode("utf-8"))
-        
+
         task_id = _tree.attrib["id"]
+        # needed? Not passed on yet ****TODO*****        
         language_pair = '{0}2{1}'.format(_tree.attrib["source-language"],
              _tree.attrib["target-language"])
         if args.dry_run_enabled:
-                    _ = EvaluationTask(task_id=task_id, task_xml=_task_file)
-            
+             _ = EvaluationTask(task_xml=_task_file, task_name=task_id, task_type="7")
         else:
-                    t = EvaluationTask(task_xml=_task_file, task_name="nowyes", task_type="7")
-                    try:  # trying to catch the exception
-                       t.save()
-                    except SuspiciousOperation as e :
-					   print e
-        
-         
-#        for _child in _tree:        
-#            task_id = _child.attrib["id"]
-        
-            # Hotfix potentially wrong ISO codes;  we are using ISO-639-3.
-            # iso_639_2_to_3_mapping = {'cze': 'ces', 'fre': 'fra', 'ger': 'deu'}
-            # for part2_code, part3_code in iso_639_2_to_3_mapping.items():
-            #    language_pair = language_pair.replace(part2_code, part3_code)
-        
-#            try:
-#                _total = _total + 1
-#                _task_xml = tostring(_child, encoding="utf-8").decode('utf-8')
-          
-                # This code to be checked to use EvaluationTask, etc.
-                # Are all relevant parameters for EvaluationTask OK?
-#                if args.dry_run_enabled:
-#                    _ = EvaluationTask(task_id=task_id, task_xml=_task_xml,
-#                      language_pair=language_pair)
+			        # making the task_name equal to the task_id
+             t = EvaluationTask(task_xml=_task_file, task_name=task_id, task_type="7")
+             try:  # trying to catch the exception
+                 _total=_total + 1
+                 t.save()
+             except Exception, msg:
+                print msg
+                _errors = _errors + 1
             
-#                else:
-                    # Use get_or_create() to avoid exact duplicates?
-                    # This code to be checked to use  EvaluationTask, etc
-#                    t = EvaluationTask(task_id=task_id, task_xml=_task_xml,
-#                      language_pair=language_pair)
-#                    t.save()
-        
-            # pylint: disable-msg=W0703
-#            except Exception, msg:
-#                print msg
-#                _errors = _errors + 1
-    
         print
         print '[{0}]'.format(_task_file)
         print 'Successfully imported {0} tasks, encountered errors for ' \
           '{1} tasks.'.format(_total, _errors)
         print
+	
